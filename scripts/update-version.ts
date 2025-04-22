34dc85eccfecd4c8755cde0dbef44d87e9f80333
Shehu-Fatiudeen Lawal <shehufatiudeen@gmail.com>
2025-04-22 04:34:49 +0100

    chore: add custom versioning and GitHub release step to deployment workflow

---
+++ scripts/update-version.ts
@@ -0,0 +1,37 @@
+import * as fs from "fs";
+import * as path from "path";
+
+function updateVersion(customVersion?: string) {
+  // Read package.json
+  const packagePath = path.join(__dirname, "../package.json");
+  const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf8"));
+
+  let newVersion: string;
+
+  if (customVersion) {
+    // Use custom version if provided
+    newVersion = customVersion;
+  } else {
+    // Parse current version
+    const [major, minor, patch] = packageJson.version.split(".").map(Number);
+    // Increment patch version
+    newVersion = `${major}.${minor}.${patch + 1}`;
+  }
+
+  // Update package.json
+  packageJson.version = newVersion;
+  fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
+
+  console.log(`Updated version to ${newVersion}`);
+  return newVersion;
+}
+
+// Parse command line arguments
+const args = process.argv.slice(2);
+const customVersion = args.includes("--set-version")
+  ? args[args.indexOf("--set-version") + 1]
+  : undefined;
+
+// Update version and export it for use in workflow
+const newVersion = updateVersion(customVersion);
+process.stdout.write(newVersion);
